5fdb4ee595f631b7408d92fc711d37414f59e8f5
woooyoung <axdsw121@gmail.com>
2022-06-22 20:11:42 +0900

    day5 - 언론사별 뉴스 가져오기

---
+++ main.py
@@ -0,0 +1,13 @@
+import journal_scrap as js
+import news_scrap as ns
+
+# 1. 언론사 목록 가져오기
+jlist = js.get_all_journal_list()
+js.print_journal_list(jlist)
+
+# 2. 언론사별 뉴스 가져오기
+journal = jlist[0]
+news_list = ns.get_new_list_by_journal(journal)
+
+print(news_list)
+
